04df22df0d84783034b4fd22bbdeac99c001a3b0
Ciro Sannino <ciro.sannino9@studenti.unina.it>
2021-11-15 13:13:42 +0100

    // Edit README and cleaned code in string_processing

--- string_processing.py
+++ string_processing.py
@@ -15,15 +15,6 @@ def time_to_seconds(time_string) -> float:
     seconds = a_timedelta.total_seconds()
     return seconds
 
-def convert_comma_numbers(s):
-    pattern = re.compile("(\d+,\d+)")
-    if pattern.match(s):
-        string = s.replace(',', '.')
-        return string
-    else:
-        return s
-
-
 def text_num_2_str(text, language) -> str:
     """
     converts a text containing numbers to a text with those numbers converted to strings in the given language. For now IT and EN are supported
@@ -35,23 +26,9 @@ def text_num_2_str(text, language) -> str:
     new_text = re.sub('[-+]?[.]?[\d]+(?:,\d\d\d)*[\.]?\d*(?:[eE][-+]?\d+)?', lambda x: num2words.num2words(int(x.group()), lang=language) if x.group().isdigit() else num2words.num2words(float(convert_comma_numbers(x.group())), lang=language) , text)
     return new_text
 
-
-# def is_number(s): # determines if input is a number or not
-#     if s.lower() == 'nan':
-#       return False
-#     else:
-#       try:
-#         string = convert_comma_numbers(s)
-#         float(string)
-#         return True
-#       except ValueError:
-#         return False
-
 def process_string(sentence, language):
   # 1. convert the numbers to string with the assigned language
   new_sentence = text_num_2_str(sentence, language)
   new_sentence = re.sub('[^0-9a-zA-Z.,!?\s]+', '', new_sentence)
   new_sentence_without_double_spaces = re.sub(' +', ' ', new_sentence)
   return new_sentence_without_double_spaces
-
-  # 2. remove any symbols that are not useful for the training.
